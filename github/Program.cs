981246c57e3440b0ca563eafafca82257aaeac16
Boldizsár Stefán <stefanboldizsar2007@gmail.com>
2022-02-09 13:51:29 +0100

    Add project files.

---
+++ github/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace github
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("knock knock");
+            string answear = Console.ReadLine();
+            if (answear=="who's there")
+            {
+
+            }
+        }
+    }
+}
